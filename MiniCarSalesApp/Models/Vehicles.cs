2ea3a2bd44f0cd39dd7d40ee15447337e9949def
Chris Hurt <chrishcoding@gmail.com>
2019-10-06 15:12:59 +1100

    Added model for Vehicle and Car

---
+++ MiniCarSalesApp/Models/Vehicles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MiniCarSalesApp
+{
+    public enum VehicleType
+    {
+        undefined, car, boat, bike
+    }
+    [Serializable]
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public VehicleType Type { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+    [Serializable]
+    public class Car : Vehicle
+    {
+        public string BodyType { get; set; }
+        public string Engine { get; set; }
+        public int Doors { get; set; }
+        public int Wheels { get; set; }
+    }
+}
